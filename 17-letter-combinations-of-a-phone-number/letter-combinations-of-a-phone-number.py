46082e9cd05fe79a406f5f22f98e9709030f3b82
Kushal Sachdeva <kushal.sach78@gmail.com>
2025-10-06 22:29:50 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (42.56%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,23 +1,19 @@
-from typing import List
-
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
+    def letterCombinations(self, digits):
         if not digits:
             return []
-        
-        mapping = {
+        mp = {
             "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
             "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
         }
-        
         res = []
 
-        def backtrack(index: int, path: str):
-            if index == len(digits):
+        def backtrack(i, path):
+            if i == len(digits):
                 res.append(path)
                 return
-            for c in mapping[digits[index]]:
-                backtrack(index + 1, path + c)
+            for c in mp[digits[i]]:
+                backtrack(i + 1, path + c)
 
         backtrack(0, "")
         return res
